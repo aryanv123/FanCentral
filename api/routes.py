22f15237804c1ee61062207d02fbf86a1a2b5a12
aryanv123 <aryanv@uw.edu>
2021-03-02 22:05:34 -0800

    Initialized and planned api

---
+++ api/routes.py
@@ -0,0 +1,28 @@
+
+# Get social feed
+def social_feed():
+    return None
+
+# Make a post
+def new_post():
+    return None
+
+# Get user porfile
+def user_profile():
+    return None
+
+# Get team profile
+def team_profile():
+    return None
+
+# Get live scores
+def live_scores():
+    return None
+
+# Get news updates
+def news_updates():
+    return None
+
+# Get chatrooms
+def chat_room():
+    return None

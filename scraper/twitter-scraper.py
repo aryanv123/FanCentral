c1c1f0be6d075427d4d13a951378421669acaabc
Eric Park <ep829@uw.edu>
2021-04-11 21:08:33 -0700

    Added Twitter Scraper

---
+++ scraper/twitter-scraper.py
@@ -0,0 +1,16 @@
+import tweepy
+
+consumer_key = "aNmee6giDyVSo42ecsO09UFe2"
+consumer_secret = "lZh278olLZ1lhzh2qVpYHusdkF6eOcVwWRif2mizeFZwvZCuUB"
+access_key = "237514533-ORcaNLwxIjrAKJSCwXeqTk4nWu7e8xgJUYVDCe0D"
+access_secret = "T26H9nZNBQb5x7swkFIUeMNYP4x6MbxWplz47Uk1s7pRI"
+
+if __name__ == '__main__':
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    new_tweets = api.user_timeline(screen_name="UW_MBB", count=10)
+
+    for tweet in new_tweets:
+        print("https://twitter.com/twitter/statuses/" + str(tweet.id))

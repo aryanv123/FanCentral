b84086412ceae70a0c46fc69214b2e1d40070b6e
aryanv123 <aryanv@uw.edu>
2021-04-15 17:21:00 -0700

    Finished reddit scraper with links

---
+++ scraper/reddit-scraper.py
@@ -0,0 +1,18 @@
+import praw
+
+reddit = praw.Reddit(client_id="WEme202K__afgw",      # your client id
+                     client_secret="mtUtN1jj7zYOeF8cdhwxybahkHTuXQ",  #your client secret
+                     user_agent="sports scraper", #user agent name
+                     username = "Own_Assistance6905")     # your reddit username
+
+subreddit = reddit.subreddit('Huskies')
+
+print(subreddit)
+
+reddit_posts = []
+
+for post in subreddit.new(limit=10):
+    reddit_posts.append("htts://www.reddit.com" + post.permalink)
+
+print(reddit_posts)
+
